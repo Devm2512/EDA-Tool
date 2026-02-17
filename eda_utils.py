3c7926ee2518b3174ce8eef79da38f5ee9fda721
Dewang Moghe <110902868+Devm2512@users.noreply.github.com>
2026-02-17 19:26:18 +0530

    Update eda_utils.py
    Added the following things in the code
    In outlier Detection - Updated the code for normal distribution
    
    Describing numeric function - function now returns the value of mean and standard deviation. and function returns the values of mean ,std dev and skew
    
    Calcualte skew and kde plot - This function now return the skew value
    
    In main.py the code now works for a single column in a dataframe.

--- eda_utils.py
+++ eda_utils.py
@@ -10,6 +10,13 @@ def calculate_skew_and_plot_kdeplot(dataframe, column):
     skew_values = dataframe[column].skew()
     print(f"\033[1mThe Skew for the column is: \033[0m", skew_values)
 
+    if abs(skew_values) <= 0.5:
+        print("\033[1mAlmost Normal Distribution\033[0m]")
+    elif abs(skew_values) > 0.5 and abs(skew_values) <= 1:
+        print("Moderately Skewed But can be \033[1maccepted as Normal Distrubiton\033[0m]")
+    else:
+        print("\033[1mHighly Skewed Distribution\033[0m")
+
     # Calculating the kdeplot
     plt.title(f"The distribution of {column}")
     sns.kdeplot(data = dataframe, x = column)
@@ -17,35 +24,47 @@ def calculate_skew_and_plot_kdeplot(dataframe, column):
     plt.ylabel("Probability Density")
     plt.show()
 
+    return skew_values
 
 
-def outlier_detection(dataframe, column):
 
-    # Computing the quatiles
+def outlier_detection(dataframe, column, skew_value, mean, std_dev):   # updating a new parameter call skew_value
+
     
-    Q1 = np.quantile(dataframe[column],0.25)
-    Q2 = np.quantile(dataframe[column],0.5)
-    Q3 = np.quantile(dataframe[column],0.75)
+    if skew_value <= 1:
+        Lower_Bound = mean - 3 *std_dev
+        Upper_Bound = mean + 3 *std_dev
 
-    print(f"\033[1mThe 25th Percentile of the {column} is : \033[0m", Q1)
-    print(f"\033[1mThe 50th Percentile of the {column} is : \033[0m", Q2)
-    print(f"\033[1mThe 75th Percentile of the {column} is : \033[0m", Q3)
+        print(Lower_Bound, Upper_Bound)
+        print("\033[1mTotal Outliers Detected: \033[0m",dataframe[(dataframe[column] < Lower_Bound) | (dataframe[column] > Upper_Bound)].shape[0])
 
-    # Computing the IQR --> Interquartile Range
 
-    IQR = Q3 - Q1
+    elif skew_value > 1:
 
-    print(f"\033[1mThe Interquartile Range of the {column} is : \033[0m", IQR)
+        # Computing the quatiles
+        Q1 = np.quantile(dataframe[column],0.25)
+        Q2 = np.quantile(dataframe[column],0.5)
+        Q3 = np.quantile(dataframe[column],0.75)
 
-    # computing the lower Bound and Upper Bound
+        print(f"\033[1mThe 25th Percentile of the {column} is : \033[0m", Q1)
+        print(f"\033[1mThe 50th Percentile of the {column} is : \033[0m", Q2)
+        print(f"\033[1mThe 75th Percentile of the {column} is : \033[0m", Q3)
 
-    Lower_Bound = Q1 - (1.5 * IQR)
-    Upper_Bound = Q3 + (1.5 * IQR)
+        # Computing the IQR --> Interquartile Range
 
-    print(f"\033[1mThe Lower_Bound of the {column} is : \033[0m", Lower_Bound)
-    print(f"\033[1mThe Upper_Bound of the {column} is : \033[0m", Upper_Bound)
+        IQR = Q3 - Q1
 
-    print("\033[1mTotal Outliers Detected: \033[0m",dataframe[dataframe[column] > Upper_Bound].shape[0])
+        print(f"\033[1mThe Interquartile Range of the {column} is : \033[0m", IQR)
+
+        # computing the lower Bound and Upper Bound
+
+        Lower_Bound = Q1 - (1.5 * IQR)
+        Upper_Bound = Q3 + (1.5 * IQR)
+
+        print(f"\033[1mThe Lower_Bound of the {column} is : \033[0m", Lower_Bound)
+        print(f"\033[1mThe Upper_Bound of the {column} is : \033[0m", Upper_Bound)
+
+        print("\033[1mTotal Outliers Detected: \033[0m",dataframe[dataframe[column] > Upper_Bound].shape[0])
 
 
     # new_df = dataframe[dataframe[column] < Upper_Bound]
@@ -60,13 +79,17 @@ def Describing_numeric_column(dataframe,column):
     print(f"\033[1mThe Minimum Value in the Column is: \033[0m", dataframe[column].min())
     print(f"\033[1mThe Standard Deviation for the Column is: \033[0m", np.std(dataframe[column]))
 
+    return dataframe[column].mean(), np.std(dataframe[column])
+
 
 
 def numeric_information(dataframe, column):
 
-    Describing_numeric_column(dataframe, column)
-    calculate_skew_and_plot_kdeplot(dataframe, column)
-    outlier_detection(dataframe, column)
+    mean, std_dev = Describing_numeric_column(dataframe, column)
+    skew = calculate_skew_and_plot_kdeplot(dataframe, column)
+    outlier_detection(dataframe, column, skew, mean, std_dev)
+
+    return mean, std_dev, skew
 
 
 
