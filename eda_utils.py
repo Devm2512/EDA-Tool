563786a43c020a18e972b1acb1c71efd355dcd2d
Dewang Moghe <110902868+Devm2512@users.noreply.github.com>
2026-03-08 13:30:18 +0530

    Update eda_utils.py
    Updating Functions

--- eda_utils.py
+++ eda_utils.py
@@ -22,17 +22,68 @@ def basic_info_dataframe(dataframe):
     return shape, column_and_its_information, sample_rows
 
 
-def numeric_information(dataframe, column):
+def rec_fuction(dataframe):
 
     basic_info_dataframe(dataframe)
-    mean, std_dev = Describing_numeric_column(dataframe, column)
-    skew = calculate_skew_and_plot_kdeplot(dataframe, column)
-    outlier_detection(dataframe, column, skew, mean, std_dev)
-    missing_value_calculation(dataframe, column)
-    whether_to_impute_missing_values(dataframe, column)
-    
+    print("The available columns are: ", dataframe.columns)
+    column = input("Enter the column name from the above list: ")
 
-    return mean, std_dev, skew
+    return column
+
+
+def numeric_information_selection(dataframe, column):
+
+    dict = {1 : "basic_info_dataframe", 2:"Describing_numeric_column",3:"calculate_skew_and_plot_kdeplot",4: "outlier_detection",
+            5: "missing_value_calculation", 6: "whether_to_impute_missing_values"}
+    
+    Enter_Operation = int(input("""Press 1 for basic_info_dataframe
+                            Press 2 for Describing_numeric_column
+                            Press 3 for calculate_skew_and_plot_kdeplot
+                            Press 4 for outlier_detection
+                            Press 5 for missing_value_calculation
+                            Press 6 for whether_to_impute_missing_values
+                            """))
+    return Enter_Operation
+
+def numeric_information(dataframe, column):
+
+    while True:
+            
+        try:
+            choice = numeric_information_selection(dataframe, column)
+
+            if choice == 1:
+                basic_info_dataframe(dataframe)
+
+            
+            elif choice == 2:
+                mean, std_dev = Describing_numeric_column(dataframe, column)
+                return mean, std_dev
+            
+            elif choice == 3:
+                skew = calculate_skew_and_plot_kdeplot(dataframe, column)
+                return skew
+            
+            elif choice == 4:
+                mean, std_dev = Describing_numeric_column(dataframe, column)
+                skew = calculate_skew_and_plot_kdeplot(dataframe, column)
+                outlier_detection(dataframe, column, skew, mean, std_dev)
+            
+            elif choice == 5:
+                missing_value_calculation(dataframe, column)
+            
+            elif choice == 6:
+                whether_to_impute_missing_values(dataframe, column)
+
+            else:
+                print("Invalid Selection")
+
+        
+        except ValueError:
+
+            print("Please Enter a Valid Number")
+
+            # return mean, std_dev, skew
 
 
 def categorical_information(dataframe, column):
