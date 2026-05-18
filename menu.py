9e891bc8e94f7071bb6b3fd964f35750175f516c
Dewang Moghe <110902868+Devm2512@users.noreply.github.com>
2026-05-18 20:51:37 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,69 +1,23 @@
-# from eda_utils import DataframeFunction
-
-# class Menu:
-
-#     # def __init__(self, location):
-
-#     #     self.location = location
+import pandas as pd
+from menu import Menu
 
 
-#     def menu(self, DataframeFunction):
+def data_reading(file = None):
 
-#         user_input = ("""What would you like to do
-#                       1. Basic Information about the data
-#                       2. Information about a particular column
-#                     """)
-        
-#         if user_input == "Basic Information about the data":
+    if file is not None:
+        dataframe = pd.read_csv(file)
 
-#             self.basic_info_dataframe()
-        
+    else:
+        path = input("Select teh csv File")
+        dataframe = pd.read_csv(path)
 
-from eda_utils import basic_info_dataframe, rec_fuction, numeric_information, numeric_information_selection
-from categorical_information import counting_information
+    return dataframe
 
-class Menu:
 
-    def __init__(self, dataframe):
-        self.dataframe = dataframe
+if __name__ == "__main__":
 
-    def start(self):
+    dataframe = data_reading()
 
-        while True:
+    menu = Menu(dataframe)
 
-            print("")
-            print("------------- EDA MENU -------------")
-            print("1. Basic Information about Data")
-            print("2. Information about a Particular Column")
-            print("3. Exit")
-            print("------------------------------------")
-
-            choice = input("Enter your choice: ")
-
-            if choice == "1":
-
-                basic_info_dataframe(self.dataframe)
-
-            elif choice == "2":
-
-                column = rec_fuction(self.dataframe)
-
-                datatype = ["int32", "int64", "float32", "float64"]
-
-                if self.dataframe[column].dtype in datatype:
-
-                    # numeric_information_selection(self.dataframe, column)
-                    numeric_information(self.dataframe, column)
-
-                else:
-
-                    counting_information(self.dataframe, column)
-
-            elif choice == "3":
-
-                print("Exiting Program...")
-                break
-
-            else:
-
-                print("Invalid Choice. Please try again.")
+    menu.start()
