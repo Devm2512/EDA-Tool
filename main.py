67c7df78f302d5051a8003a2d6941e05c171bcd1
Dewang Moghe <110902868+Devm2512@users.noreply.github.com>
2026-05-18 20:51:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import pandas as pd
 from menu import Menu
 
 
-def data_reading():
+def data_reading(file = None):
 
-    dataframe = pd.read_csv("C:/Users/dewan/Downloads/data_science/EDA Project/df_selected_cols.csv")
+    if file is not None:
+        dataframe = pd.read_csv(file)
+
+    else:
+        path = input("Select teh csv File")
+        dataframe = pd.read_csv(path)
 
     return dataframe
 
